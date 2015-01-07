c5eb446f76df6e0edc94df1e6fd40e325b84475b
jgshumate1 <jgshumate1@gmail.com>
2015-01-06 21:11:33 -0500

    Initial code commit.

---
+++ KeyCode/Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace KeyCode
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxInput = new System.Windows.Forms.TextBox();
+            this.KeyCodeStatus = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBoxInput
+            // 
+            this.textBoxInput.Location = new System.Drawing.Point(113, 77);
+            this.textBoxInput.Name = "textBoxInput";
+            this.textBoxInput.Size = new System.Drawing.Size(100, 20);
+            this.textBoxInput.TabIndex = 0;
+            this.textBoxInput.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBoxInput_KeyDown);
+            // 
+            // KeyCodeStatus
+            // 
+            this.KeyCodeStatus.AutoSize = true;
+            this.KeyCodeStatus.Location = new System.Drawing.Point(327, 96);
+            this.KeyCodeStatus.Name = "KeyCodeStatus";
+            this.KeyCodeStatus.Size = new System.Drawing.Size(35, 13);
+            this.KeyCodeStatus.TabIndex = 1;
+            this.KeyCodeStatus.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(654, 284);
+            this.Controls.Add(this.KeyCodeStatus);
+            this.Controls.Add(this.textBoxInput);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxInput;
+        private System.Windows.Forms.Label KeyCodeStatus;
+    }
+}
+
---
+++ KeyCode/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace KeyCode
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnKeyPress(KeyPressEventArgs e)
+        {
+            base.OnKeyPress(e);
+            if (char.IsLetter(e.KeyChar))
+            {
+                // char is letter
+            }
+            else if (char.IsDigit(e.KeyChar))
+            {
+                // char is digit
+            }
+            else
+            {
+                // char is neither letter or digit.
+                // there are more methods you can use to determine the
+                // type of char, e.g. char.IsSymbol
+            }
+        }
+
+        private void textBoxInput_KeyDown(object sender, KeyEventArgs e)
+        {
+            // Determine whether the keystroke is a number from the top of the keyboard. 
+            if (e.KeyCode >= Keys.D0 && e.KeyCode <= Keys.D9)
+            {
+                KeyCodeStatus.Text = e.KeyCode.ToString();
+            }
+            else if (e.KeyCode >= Keys.NumPad0 && e.KeyCode <= Keys.NumPad9)
+            {
+                KeyCodeStatus.Text = e.KeyCode.ToString();
+            }
+            else if (e.KeyCode == Keys.Back)
+            {
+                KeyCodeStatus.Text = "Back key pressed";
+            } else if (e.KeyCode >= Keys.A && e.KeyCode <= Keys.Z)
+            {
+                KeyCodeStatus.Text = e.KeyCode.ToString();
+            }
+            else
+            {
+                KeyCodeStatus.Text = e.KeyCode.ToString();
+            }
+
+            //If shift key was pressed, it's not a number. 
+            if (Control.ModifierKeys == Keys.Shift)
+            {
+                KeyCodeStatus.Text = KeyCodeStatus.Text + "Shift Key is pressed";
+            }
+        }
+    }
+}
